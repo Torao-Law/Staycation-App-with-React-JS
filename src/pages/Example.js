029c2d53be62d10198f7526c9b4c185847953f57
Dandi Saputra <67894946+Torao-Law@users.noreply.github.com>
2021-10-26 15:20:38 +0700

    Create component breadcrumb

--- src/pages/Example.js
+++ src/pages/Example.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react'
 
-import InputDate from "../elements/form/inputDate/index"
+import Breadcrumb from "../elements/Breadcrumb/index"
 
 export default class Example extends Component{
-    state = {
-        value: {
-            startDate: new Date(),
-            endDate: new Date(),
-            key: "selection"
-        }
-    }
-
-    handleChange = e => {
-        this.setState({ [e.target.name]: e.target.value});
-    }
-
     render() {
+        const breadcrumbList = [
+            {pageTitle: "Home", pageHref:""},
+            {pageTitle: "House Details", pageHref:""},
+        ]
+
         return(
             <div className="container">
                 <div className="row align-items-center justify-content-center" style={{ height: "70vh" }}>
                     <div className="col-auto">
-                        <InputDate  max={30} onChange={this.handleChange} name="value" value={this.state.value} />
+                        <Breadcrumb data={breadcrumbList} />
                     </div>
                 </div>
             </div>
